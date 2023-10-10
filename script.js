9b367fc4e49418b829cc77ff36ba5b85ea85a6fe
JinjingZhang1208 <zhang.jinjin@northeastern.edu>
2023-10-09 21:05:52 -0700

    update my homepage

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ function toggleClass() {
     element.classList.toggle("zoom-comment-box");
 }
 
+function addYellowBackground() {
+    const body = document.body;
+    body.classList.add("yellow-background");
+}
+
+function removeYellowBackground() {
+    const body = document.body;
+    body.classList.remove("yellow-background");
+}
+
+const changeColorButton = document.querySelector("#changeColorButton");
+
+changeColorButton.addEventListener("click", function () {
+    const body = document.body;
+
+    if (body.classList.contains("yellow-background")) {
+        removeYellowBackground();
+    } else {
+        addYellowBackground();
+    }
+});
+
 function addComment(event) {
     if (event.key === "Enter" || event.type === "click") {
         const commentInput = document.querySelector("#commentInput");
-        // remove whitespace characters from the beginning and end of a string.
         const commentText = commentInput.value.trim();
 
         if (commentText !== "") {
